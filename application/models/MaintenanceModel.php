d3630a8ea85f8e3d5b86a84c82b964cfe142481b
suchananMU <64394044+suchananMU@users.noreply.github.com>
2022-04-04 19:16:34 +0700

    update

--- application/models/MaintenanceModel.php
+++ application/models/MaintenanceModel.php
@@ -5,11 +5,11 @@ class MaintenanceModel extends CI_Model {
     {
             //$query = $this->db->get('users'); // SELECT * FROM users
             
-            $query = $this->db->query("SELECT m.maintenanceID, m.date, m.details, mt.maintenancetypeID ,mt.n_maintenancetype, mt.detail, mt.recommend, e.n_prefix, e.f_name, e.l_name, v.n_common_TH 
-            FROM ((((maintenance m INNER JOIN maintenancetype mt ON m.maintenancetype_maintenancetypeID = mt.maintenancetypeID) 
+            $query = $this->db->query("SELECT * FROM (((((maintenance m INNER JOIN maintenancetype mt ON m.maintenancetype_maintenancetypeID = mt.maintenancetypeID) 
             INNER JOIN employee e ON m.employee_id_employee = e.employeeID) 
             INNER JOIN plants ON m.plants_plantID = plants.plantID) 
-            INNER JOIN vegetation v ON plants.vegetation_vegetationID = v.vegetationID);");
+            INNER JOIN vegetation v ON plants.vegetation_vegetationID = v.vegetationID)
+            INNER JOIN prefix  ON prefix.PrefixID = e.PrefixID);");
             return $query->result();
     }
 }
